845a311f13502f034acaae4222c9a895a69306a4
luckyteos <luckyteos@users.noreply.github.com>
2021-10-24 10:27:35 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,6 +8,28 @@ import json
 import asyncio
 import time
 
+# Use this function to get the more accurate time of a transaction, instead of timestamp in transaction
+# Inputs: Transaction Hash from event
+# Outputs: Transaction timestamp
+# def get_transaction_time(tx_hash):
+#     tx = chain.get_transaction(tx_hash)
+#     return tx.timestamp
+
+# Helper function
+# Deploys a contracts and writes its abi to abi.json
+def deploy_and_get_abi():
+    ByteABlock.deploy({'from':accounts[0], 'gas_price': 0})
+    with open("abi.json", "w") as f:
+        json.dump(ByteABlock[0].abi, f, indent=4)
+
+# Gets address of the deployed contract, usually the first block
+def get_deployed_contract_addr():
+    deploy_tx = web3.eth.get_transaction_by_block(1, 0)
+    deploy_tx_hash = deploy_tx["hash"]
+    tx = chain.get_transaction(deploy_tx_hash)
+    return tx.contract_address
+
+
 # Connect to the blockchain network
 # Add a network to brownie through brownie networks add BROWNIE_NETWORK_GROUP BROWNIE_NETWORK_ID host=http://NODE_IP:NODE_PORT chainid=NETWORK_ID name=FRIENDLY_NETWORK_NAME
 # i.e. brownie networks add live VPN host=http://10.7.0.6:8501 chainid=10117 name="VPN Network"
@@ -39,6 +61,7 @@ print(contract.decode_input(tx.input))
 print(tx.events)
 print(tx.logs)
 print(tx.status)
+print(tx.timestamp)
 print()
 
 # Retrieve items from the chain using centralStore getter
@@ -49,7 +72,7 @@ print(contract.centralStore(1,1)[0])
 print(contract.centralStore(1,1)[1])
 print()
 
-# Add an evidence item to the chain (emits evidenceAdded)
+# Add an evidence item to the chain
 # gas_price must be at least one if not transaction will fail due to insufficient gas (applies to all transactions)
 # addEvidenceItem(
 #        uint _caseId, uint _evidId, uint _handlerId,
@@ -57,11 +80,13 @@ print()
 #        string memory _serialNo, string memory _model, 
 #        string memory _currStatus, string memory _purpose, string memory _notes
 #        )
+# emits evidenceAdded(_caseId, _evidId, _handlerId, _currStatus, _purpose, block.timestamp)
 # Remove the .call after the method name to execute the actual transaction
 contract.addEvidenceItem.call(1,3,1,"SIT","4523agvc","Storage","4673","WD SN550","In Transit","","Device is operational", {'from': accounts[0], 'gas_price':1})
 
 # Change the status of an existing evidence item
 # modifyEvidenceStatus(uint _caseId, uint _evidId, uint _handlerId, string memory _newStatus, string memory _purpose)
+# emit statusChanged(_caseId, _evidId, _handlerId, _newStatus, _purpose, block.timestamp)
 # Remove the .call after the method name to execute the actual transaction
 contract.modifyEvidenceStatus.call(1,1,2,"For Storage","Check-In for Storage",{'from': accounts[0], 'gas_price':1})
 
@@ -77,7 +102,7 @@ contract_w3 = web3.eth.contract(address="0x1433bE399CF5a9C44B9C76bc148CA188dCaFA
 add_item_filter = contract_w3.events.evidenceAdded.createFilter(fromBlock=1, argument_filters={"_caseId":1})
 change_filter = contract_w3.events.statusChanged.createFilter(fromBlock=1, argument_filters={"_caseId": 1, "_evidId": 1})
 
-# Get the results that you want after setting up the filters, results will differ based on what filter is it for, and when results are retrieved
+# Get the results that you want after setting up the filters, results will differ based on what filter is it for, and results are retrieved
 # Use get_new_entries() to only get new changes since the block specified in fromBlock
 # Use get_all_entries() to get all changes since fromBlock specified in filter
 # Reference web3.py documentations on how to call these in a loop, best in an asynchronous manner
@@ -89,4 +114,5 @@ print(added_items)
 print(added_items[0]["args"]["_caseId"])
 print(added_items[1]["args"])
 print()
+
 #print(changed_items)
