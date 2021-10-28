d9772d02c46816d3b9bd597105d96aaf6569d2f8
luckyteos <luckyteos@users.noreply.github.com>
2021-10-29 00:17:35 +0800

    Update ByteABlock.sol

--- ByteABlock.sol
+++ ByteABlock.sol
@@ -17,8 +17,8 @@ contract ByteABlock {
     
     mapping(uint => mapping(uint => evidenceItem)) public centralStore;
     
-    event evidenceAdded(uint indexed _caseId, uint indexed _evidId, uint indexed _handlerId, string _currStatus, string _purpose, uint256 date);
-    event statusChanged(uint indexed _caseId, uint indexed _evidId, uint indexed _handlerId, string _currStatus, string _purpose, uint256 date);
+    event evidenceAdded(uint indexed _caseId, uint indexed _evidId, uint indexed _handlerId, string _currStatus, string _serialNo, uint256 date);
+    event statusChanged(uint indexed _caseId, uint indexed _evidId, uint indexed _handlerId, string _currStatus, string _imageHash, string _purpose, uint256 date);
     
     function addEvidenceItem(
         uint _caseId, uint _evidId, uint _handlerId,
@@ -27,16 +27,15 @@ contract ByteABlock {
         string memory _currStatus, string memory _purpose, string memory _notes
         ) public {
             centralStore[_caseId][_evidId] = evidenceItem(_handlerId, _location, _imageHash, _evidType, _serialNo, _model, _currStatus, _purpose, _notes);
-            emit evidenceAdded(_caseId, _evidId, _handlerId, _currStatus, _purpose, block.timestamp);
+            emit evidenceAdded(_caseId, _evidId, _handlerId, _currStatus, _serialNo, block.timestamp);
         }
         
-    function modifyEvidenceStatus(uint _caseId, uint _evidId, uint _handlerId, string memory _newStatus, string memory _purpose) public{
+    function modifyEvidenceStatus(uint _caseId, uint _evidId, uint _handlerId, string memory _newStatus, string memory _imageHash, string memory _purpose) public{
         centralStore[_caseId][_evidId].currStatus = _newStatus;
         centralStore[_caseId][_evidId].handlerId = _handlerId;
+        centralStore[_caseId][_evidId].imageHash = _imageHash;
         centralStore[_caseId][_evidId].purpose = _purpose;
-        emit statusChanged(_caseId, _evidId, _handlerId, _newStatus, _purpose, block.timestamp);
+        emit statusChanged(_caseId, _evidId, _handlerId, _newStatus, _imageHash, _purpose, block.timestamp);
     }
     
 }
-
-
